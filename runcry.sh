d769cba57a2c20f83985542af21e10bd4113b353
Barbara M. T. C. Peluzo <bmpeluzo.smu@gmail.com>
2024-07-11 16:58:57 -0400

    Script ready

--- runcry.sh
+++ runcry.sh
@@ -1,390 +1,389 @@
 #! /bin/bash
 
-while getopts id:c: flag
+while getopts j:c:r:f:F:n:p: flag
 do
 	case ${flag} in
-		id ) job_id=${OPTARG};;
+		j ) job_id=${OPTARG};;
 		c ) cores=${OPTARG};;
-		#n) nodes=${OPTARG};;
-		#o) out=${OPTARG};;
-		#p) part=${OPTARG};;
-		#e) prop=${OPTARG};;
-		#f) file=${OPTARG};;
+		r ) proj=${OPTARG};;
+		n) nodes=${OPTARG};;
+		p) part=${OPTARG};;
+		f) fort_file=${OPTARG};;
+		F) other_file=${OPTARG};;
 	esac
 done
 
+########## setting default values #############3
+if [ -z $nodes ]; then
+	nodes=1
+fi
 
+if [ -z $proj ]; then
+	job_dir='${SCRATCH}/${SLURM_JOB_ID}'
+else
+	job_dir='${SCRATCH}/'${proj}'/${SLURM_JOB_ID}'
+fi
+
+############## checking for required flags ############
 if [ -z $job_id ]; then
 	echo "Job name not specified"
 	exit
+elif [ -z $cores ]; then
+	echo "Number of cores not specified"
+	exit
+elif [ -z $part ]; then
+	echo "Partition not specified"
+	exit
 else
 	echo "#!/bin/bash
-#SBATCH -p teraeth -t 30-0:00:00 -o ${job_id}.out
-#SBATCH -N 1 --ntasks-per-node=${cores} --mem=0 -C Gold6448Y,ib
+#SBATCH -p ${part} -t 30-0:00:00 -o ${job_id}.out
+#SBATCH -N ${nodes} --ntasks-per-node=${cores} --mem=0 -C Gold6448Y,ib
 
 module load /scratch/mruggie8_lab/software/modulefiles/crystal/b2
 
-job_dir=${SCRATCH}/tests/${SLURM_JOB_ID}
-
-
-
-job_dir=\${SCRATCH}/\${SLURM_JOB_ID}
+job_dir=${job_dir}
 mkdir -p \${job_dir}
-export OMP_NUM_THREADS=\${SLURM_CPUS_PER_TASK}
 
+cp ${job_id}.d12 \${job_dir}/INPUT 
 
 ########## copying additional input files ###############
 
-if [ -e ${out}.f9 ]; then
-	cp ${out}.f9 \${job_dir}/fort.9
+if [ -e ${job_id}.f9 ]; then
+	cp ${job_id}.f9 \${job_dir}/fort.9
 fi
 
 if [ -e BASISSET.DAT ]; then
 	cp BASISSET.DAT \${job_dir}/
 fi
 
-if [ -e ${out}.f65 ]; then
-	cp ${out}.f65 \${job_dir}/fort.65
+if [ -e ${job_id}.f65 ]; then
+	cp ${job_id}.f65 \${job_dir}/fort.65
 fi
 
-if [ -e ${out}.f75 ]; then
-	cp ${out}.f75 \${job_dir}/fort.75
+if [ -e ${job_id}.f75 ]; then
+	cp ${job_id}.f75 \${job_dir}/fort.75
 fi
 
-if [ -e ${out}.f77 ]; then
-	cp ${out}.f77 \${job_dir}/fort.77
-fi
-
-
-if [ -e ${out}.f21 ]; then
-	cp ${out}.f21 \${job_dir}/fort.21
-fi
-
-if [ -e ${out}.f20 ]; then
-	cp ${out}.f20 \${job_dir}/fort.20
+if [ -e ${job_id}.f77 ]; then
+	cp ${job_id}.f77 \${job_dir}/fort.77
 fi
 
 
-if [ -e ${out}.f34 ]; then
-	cp ${out}.f34 \${job_dir}/fort.34
+if [ -e ${job_id}.f21 ]; then
+	cp ${job_id}.f21 \${job_dir}/fort.21
 fi
 
-if [ -e ${out}.f13 ]; then
-	cp ${out}.f13 \${job_dir}/fort.13
+if [ -e ${job_id}.f20 ]; then
+	cp ${job_id}.f20 \${job_dir}/fort.20
 fi
 
-if [ -e ${out}.f80 ]; then
-	cp ${out}.f80 \${job_dir}/fort.80
+
+if [ -e ${job_id}.f34 ]; then
+	cp ${job_id}.f34 \${job_dir}/fort.34
 fi
 
-if [ -e ${out}.freqinfo ]; then
-	cp ${out}.freqinfo \${job_dir}/FREQINFO.DAT
+if [ -e ${job_id}.f13 ]; then
+	cp ${job_id}.f13 \${job_dir}/fort.13
 fi
 
-if [ -e ${out}.tensraman ]; then
-	cp ${out}.tensraman \${job_dir}/TENS_RAMAN.DAT
+if [ -e ${job_id}.f80 ]; then
+	cp ${job_id}.f80 \${job_dir}/fort.80
 fi
 
-if [ -e ${out}.tensir ]; then
-	cp ${out}.tensir \${job_dir}/TENS_IR.DAT
+if [ -e ${job_id}.freqinfo ]; then
+	cp ${job_id}.freqinfo \${job_dir}/FREQINFO.DAT
 fi
 
-if [ -e ${out}.vibpot ]; then
-	cp ${out}.vibpot \${job_dir}/VIBPOT.DAT
+if [ -e ${job_id}.tensraman ]; then
+	cp ${job_id}.tensraman \${job_dir}/TENS_RAMAN.DAT
 fi
 
-if [ -e ${out}.born ]; then
-	cp ${out}.born \${job_dir}/BORN.DAT
+if [ -e ${job_id}.tensir ]; then
+	cp ${job_id}.tensir \${job_dir}/TENS_IR.DAT
 fi
 
-if [ -e ${out}.elasinfo ]; then
-	cp ${out}.elasinfo \${job_dir}/ELASINFO.DAT
+if [ -e ${job_id}.vibpot ]; then
+	cp ${job_id}.vibpot \${job_dir}/VIBPOT.DAT
 fi
 
-if [ -e ${out}.eosinfo ]; then
-	cp ${out}.eosinfo \${job_dir}/EOSINFO.DAT
+if [ -e ${job_id}.born ]; then
+	cp ${job_id}.born \${job_dir}/BORN.DAT
 fi
 
-if [ -e ${out}.scanpes ]; then
-	cp ${out}.scanpes \${job_dir}/SCANPES.DAT
+if [ -e ${job_id}.elasinfo ]; then
+	cp ${job_id}.elasinfo \${job_dir}/ELASINFO.DAT
 fi
 
-if [ -e ${out}.opthess ]; then
-	cp ${out}.opthess \${job_dir}/OPTHESS.DAT
+if [ -e ${job_id}.eosinfo ]; then
+	cp ${job_id}.eosinfo \${job_dir}/EOSINFO.DAT
 fi
 
-if [ -e ${out}.optinfo ]; then
-	cp ${out}.optinfo \${job_dir}/OPTINFO.DAT
+if [ -e ${job_id}.scanpes ]; then
+	cp ${job_id}.scanpes \${job_dir}/SCANPES.DAT
 fi
 
-if [ -e ${out}.hessopt ]; then
-	cp ${out}.hessopt \${job_dir}/HESSOPT.DAT
+if [ -e ${job_id}.opthess ]; then
+	cp ${job_id}.opthess \${job_dir}/OPTHESS.DAT
 fi
-" > ${out}.sbatch
 
-# It defaults to read files from units with the same name as the input, In case the user wants to specify an additional FORT file to be read (will keep updating following this logic. IT ONLY WORKS FOR FORT FILES!!!!!!!) :
+if [ -e ${job_id}.optinfo ]; then
+	cp ${job_id}.optinfo \${job_dir}/OPTINFO.DAT
+fi
 
-if [ -z ${file} ]; then
-	echo "User did not specify additional input files"
+if [ -e ${job_id}.hessopt ]; then
+	cp ${job_id}.hessopt \${job_dir}/HESSOPT.DAT
+fi
+
+cd \${job_dir}
+
+cat INPUT
+
+echo \"Job ID: \${SLURM_JOB_ID}\"
+
+mpirun  /scratch/mruggie8_lab/software/CRYSTAL/CRYSTAL_BARBARA/CRY23/bin/bluehive_impi_nodebug/std/Pcrystal
+
+" > ${job_id}.sbatch
+
+# It defaults to read files from units with the same name as the input, In case the user wants to specify an additional FORT file to be read:
+
+if [ -z ${fort_file} ]; then
+	echo "User did not specify additional f input files"
 	else
-	file_name=$(ls ${file} | cut -d. -f1)
-	file_ext=$(ls ${file} | cut -d. -f2)
+	file_name=$(ls ${fort_file} | cut -d. -f1)
+	file_ext=$(ls ${fort_file} | cut -d. -f2)
 	let size=${#file_ext} ## accessing the number of characters in the extension string
 	new_ext=$(echo ${file_ext[@]:1:${size}})
-	echo "You gave me ${file}, I will copy this file as fort.${new_ext}"
-	echo "cp ${file} \${job_dir}/fort.${new_ext} " >> ${out}.sbatch
+	echo "You gave me ${fort_file}, I will copy this file as fort.${new_ext}"
+	echo "cp ${fort_file} \${job_dir}/fort.${new_ext} " >> ${job_id}.sbatch
 fi
  
+# Other additional files:
+
+if [ -z ${other_file} ]; then
+	echo "User did not specify additional input files"
+	else
+	echo "Copying ${other_file} to job directory"
+	echo "cp ${other_file} \${job_dir}/" >> ${job_id}.sbatch
+fi
+
+
 
 #################################################################################
 
 
-## It defaults to running crystal. If the user aims to run properties, the flag -pp should be specified
 
-if [ -z ${prop} ]; then
-	echo "cp ${out}.d12 \${job_dir}/INPUT
-cd \${job_dir}
-srun PcrystalOMP < INPUT >> ${out}.out " >> ${out}.sbatch
-else
-	echo "cp ${out}.d3 \${job_dir}/INPUT
-cd \${job_dir}
-srun Pproperties < INPUT >> ${out}.out " >> ${out}.sbatch
-fi
-
-echo "
-echo \"JOD ID: \${SLURM_JOB_ID}\" >> ${out}.out 
-
-cat INPUT >> ${out}.out " >> ${out}.sbatch
-
-
-	sed -i 's/SBATCH -J job_name/SBATCH -J '${out}'/g' ${out}.sbatch
-	sed -i 's/SBATCH -o job_name/SBATCH -o '${out}'.out/g' ${out}.sbatch
 	
-	if [ -z $nodes ]; then
-		echo "Number of nodes not specified"
-	else
-		sed -i 's/SBATCH -N nodes/SBATCH -N '${nodes}'/g' ${out}.sbatch
-
-		if [ -z $part ]; then
-			echo "Partition not specified"
-		else
-			sed -i 's/SBATCH -p job_queue/SBATCH -p '${part}'/g' ${out}.sbatch
-			if [[ "${part}" == "${high}" ]]; then
-				sed -i 's/SBATCH --mem=500G/SBATCH --mem=1950GB/g' ${out}.sbatch
-			fi
-		fi
-	fi
-fi
-
 echo "
+################## copying output files ########################
+
 if [ -e KAPPA.DAT ]
 then
-   cp KAPPA.DAT \${SLURM_SUBMIT_DIR}/${out}.KAPPA.DAT
+   cp KAPPA.DAT \${SLURM_SUBMIT_DIR}/${job_id}.KAPPA.DAT
 fi
 
 if [ -e POWER_C.DAT ]
 then
-   cp POWER_C.DAT \${SLURM_SUBMIT_DIR}/${out}.POWER_C.DAT
+   cp POWER_C.DAT \${SLURM_SUBMIT_DIR}/${job_id}.POWER_C.DAT
 fi
 
 if [ -e POWER.DAT ]
 then
-   cp POWER.DAT \${SLURM_SUBMIT_DIR}/${out}.POWER.DAT
+   cp POWER.DAT \${SLURM_SUBMIT_DIR}/${job_id}.POWER.DAT
 fi
 
 if [ -e SEEBECK.DAT ]
 then
-   cp SEEBECK.DAT \${SLURM_SUBMIT_DIR}/${out}.SEEBECK.DAT
+   cp SEEBECK.DAT \${SLURM_SUBMIT_DIR}/${job_id}.SEEBECK.DAT
 fi
 
 if [ -e SIGMA.DAT ]
 then
-   cp SIGMA.DAT \${SLURM_SUBMIT_DIR}/${out}.SIGMA.DAT
+   cp SIGMA.DAT \${SLURM_SUBMIT_DIR}/${job_id}.SIGMA.DAT
 fi
 
 if [ -e SIGMAS.DAT ]
 then
-   cp SIGMAS.DAT \${SLURM_SUBMIT_DIR}/${out}.SIGMAS.DAT
+   cp SIGMAS.DAT \${SLURM_SUBMIT_DIR}/${job_id}.SIGMAS.DAT
 fi
 
 if [ -e TDF.DAT ]
 then
-   cp TDF.DAT \${SLURM_SUBMIT_DIR}/${out}.TDF.DAT
+   cp TDF.DAT \${SLURM_SUBMIT_DIR}/${job_id}.TDF.DAT
 fi
 
 if [ -e fort.34 ]
 then
-   cp fort.34 \${SLURM_SUBMIT_DIR}/${out}.f34
+   cp fort.34 \${SLURM_SUBMIT_DIR}/${job_id}.f34
 fi
 
 if [ -e fort.9 ]
 then
-   cp fort.9 \${SLURM_SUBMIT_DIR}/${out}.f9
+   cp fort.9 \${SLURM_SUBMIT_DIR}/${job_id}.f9
 fi
 
 if [ -e fort.20 ]
 then
-   cp fort.20 \${SLURM_SUBMIT_DIR}/${out}.f20
+   cp fort.20 \${SLURM_SUBMIT_DIR}/${job_id}.f20
 fi
 
 if [ -e OPTHESS.DAT ]
 then
-   cp OPTHESS.DAT \${SLURM_SUBMIT_DIR}/${out}.opthess
+   cp OPTHESS.DAT \${SLURM_SUBMIT_DIR}/${job_id}.opthess
 fi
 
 if [ -e OPTINFO.DAT ]
 then
-   cp OPTINFO.DAT \${SLURM_SUBMIT_DIR}/${out}.optinfo
+   cp OPTINFO.DAT \${SLURM_SUBMIT_DIR}/${job_id}.optinfo
 fi
 
 if [ -e XMETRO.COR ]
 then
-   cp XMETRO.COR \${SLURM_SUBMIT_DIR}/${out}.xmetro
+   cp XMETRO.COR \${SLURM_SUBMIT_DIR}/${job_id}.xmetro
 fi
 
 if  [ -e BORN.DAT ]
 then
-   cp BORN.DAT \${SLURM_SUBMIT_DIR}/${out}.born
+   cp BORN.DAT \${SLURM_SUBMIT_DIR}/${job_id}.born
 fi
 
 if  [ -e IRREFR.DAT ]
 then
-   cp IRREFR.DAT \${SLURM_SUBMIT_DIR}/${out}.irrefr
+   cp IRREFR.DAT \${SLURM_SUBMIT_DIR}/${job_id}.irrefr
 fi
 
 if  [ -e IRDIEL.DAT ]
 then
-   cp IRDIEL.DAT \${SLURM_SUBMIT_DIR}/${out}.irdiel
+   cp IRDIEL.DAT \${SLURM_SUBMIT_DIR}/${job_id}.irdiel
 fi
 
 if [ -e ADP.DAT ]
 then
-   cp ADP.DAT \${SLURM_SUBMIT_DIR}/${out}.adp
+   cp ADP.DAT \${SLURM_SUBMIT_DIR}/${job_id}.adp
 fi
 
 if [ -e ELASINFO.DAT ]
 then
-   cp ELASINFO.DAT \${SLURM_SUBMIT_DIR}/${out}.elasinfo
+   cp ELASINFO.DAT \${SLURM_SUBMIT_DIR}/${job_id}.elasinfo
 fi
 
 if [ -e EOSINFO.DAT ]
 then
-   cp EOSINFO.DAT \${SLURM_SUBMIT_DIR}/${out}.eosinfo
+   cp EOSINFO.DAT \${SLURM_SUBMIT_DIR}/${job_id}.eosinfo
 fi
 
 if [ -e GEOMETRY.CIF ]
 then
-   cp GEOMETRY.CIF \${SLURM_SUBMIT_DIR}/${out}.cif
+   cp GEOMETRY.CIF \${SLURM_SUBMIT_DIR}/${job_id}.cif
 fi
 
 if [ -e GAUSSIAN.DAT ]
 then
-   cp GAUSSIAN.DAT \${SLURM_SUBMIT_DIR}/${out}.gjf
+   cp GAUSSIAN.DAT \${SLURM_SUBMIT_DIR}/${job_id}.gjf
 fi
 
 if [ -e FINDSYM.DAT ]
 then
-   cp FINDSYM.DAT \${SLURM_SUBMIT_DIR}/${out}.FINDSYM
+   cp FINDSYM.DAT \${SLURM_SUBMIT_DIR}/${job_id}.FINDSYM
 fi
 
-if ls \$TMPDIR/opt* &>/dev/null
+if ls \${TMPDIR}/opt* &>/dev/null
 then
-  mkdir \${SLURM_SUBMIT_DIR}/${out}.optstory &> /dev/null
-  for file in \$TMPDIR/opt*
+  mkdir \${SLURM_SUBMIT_DIR}/${job_id}.optstory &> /dev/null
+  for file in \${TMPDIR}/opt*
   do
-     cp \$file \${SLURM_SUBMIT_DIR}/${out}.optstory
+     cp \$file \${SLURM_SUBMIT_DIR}/${job_id}.optstory
   done
 fi
 
 
 if [ -e fort.69 ]
 then
-   cp fort.69 \${SLURM_SUBMIT_DIR}/${out}.f69
+   cp fort.69 \${SLURM_SUBMIT_DIR}/${job_id}.f69
 fi
 
 if [ -e fort.98 ]
 then
-   cp fort.98 \${SLURM_SUBMIT_DIR}/${out}.f98
+   cp fort.98 \${SLURM_SUBMIT_DIR}/${job_id}.f98
 fi
 
 if [ -e fort.80 ]
 then
-   cp fort.80 \${SLURM_SUBMIT_DIR}/${out}.f80
+   cp fort.80 \${SLURM_SUBMIT_DIR}/${job_id}.f80
 fi
 
 if [ -e fort.13 ]
 then
-   cp fort.13 \${SLURM_SUBMIT_DIR}/${out}.f13
+   cp fort.13 \${SLURM_SUBMIT_DIR}/${job_id}.f13
 fi
 
 if [ -e fort.85 ]
 then
-   cp fort.85 \${SLURM_SUBMIT_DIR}/${out}.f85
+   cp fort.85 \${SLURM_SUBMIT_DIR}/${job_id}.f85
 fi
 
 if [ -e fort.90 ]
 then
-   cp fort.90 \${SLURM_SUBMIT_DIR}/${out}.f90
+   cp fort.90 \${SLURM_SUBMIT_DIR}/${job_id}.f90
 fi
 
 if [ -e fort.21 ]
 then
-   cp fort.21 \${SLURM_SUBMIT_DIR}/${out}.f21
+   cp fort.21 \${SLURM_SUBMIT_DIR}/${job_id}.f21
 fi
 
 if [ -e fort.75 ]
 then
-   cp fort.75 \${SLURM_SUBMIT_DIR}/${out}.f75
+   cp fort.75 \${SLURM_SUBMIT_DIR}/${job_id}.f75
 fi
 
 if [ -e FREQINFO.DAT ]
 then
-   cp FREQINFO.DAT \${SLURM_SUBMIT_DIR}/${out}.freqinfo
+   cp FREQINFO.DAT \${SLURM_SUBMIT_DIR}/${job_id}.freqinfo
 fi
 
 if [ -e TENS_IR.DAT ]
 then
-   cp TENS_IR.DAT \${SLURM_SUBMIT_DIR}/${out}.tensir
+   cp TENS_IR.DAT \${SLURM_SUBMIT_DIR}/${job_id}.tensir
 fi
 
 if [ -e IRSPEC.DAT ]
 then
-   cp IRSPEC.DAT \${SLURM_SUBMIT_DIR}/${out}_IRSPEC.DAT
+   cp IRSPEC.DAT \${SLURM_SUBMIT_DIR}/${job_id}.IRSPEC.DAT
 fi
 
 if [ -e TENS_RAMAN.DAT ]
 then
-   cp TENS_RAMAN.DAT \${SLURM_SUBMIT_DIR}/${out}.tensraman
+   cp TENS_RAMAN.DAT \${SLURM_SUBMIT_DIR}/${job_id}.tensraman
 fi
 
 if [ -e RAMSPEC.DAT ]
 then
-   cp RAMSPEC.DAT \${SLURM_SUBMIT_DIR}/${out}_RAMSPEC.DAT
+   cp RAMSPEC.DAT \${SLURM_SUBMIT_DIR}/${job_id}.RAMSPEC.DAT
 fi
 
 if [ -e SCANPES.DAT ]
 then
-   cp SCANPES.DAT \${SLURM_SUBMIT_DIR}/${out}.scanpes
+   cp SCANPES.DAT \${SLURM_SUBMIT_DIR}/${job_id}.scanpes
 fi
 
 if [ -e VIBPOT.DAT ]
 then
-   cp VIBPOT.DAT \${SLURM_SUBMIT_DIR}/${out}.vibpot
+   cp VIBPOT.DAT \${SLURM_SUBMIT_DIR}/${job_id}.vibpot
 fi
 
 if [ -e MOLDRAW.DAT ]
 then
-   cp MOLDRAW.DAT \${SLURM_SUBMIT_DIR}/${out}.mol
+   cp MOLDRAW.DAT \${SLURM_SUBMIT_DIR}/${job_id}.mol
 fi
 
 if [ -e fort.92 ]
 then
-   cp fort.92 \${SLURM_SUBMIT_DIR}/${out}.com
+   cp fort.92 \${SLURM_SUBMIT_DIR}/${job_id}.com
 fi
 
 if [ -e fort.33 ]
 then
-   cp fort.33 \${SLURM_SUBMIT_DIR}/${out}.xyz
+   cp fort.33 \${SLURM_SUBMIT_DIR}/${job_id}.xyz
 fi
 
 #----------------------------------------------------
@@ -393,39 +392,39 @@ fi
 
 if [ -e PCF.DAT ]
 then
-   cp PCF.DAT \${SLURM_SUBMIT_DIR}/PCF_${out}.DAT
+   cp PCF.DAT \${SLURM_SUBMIT_DIR}/PCF_${job_id}.DAT
 fi
 
 if [ -e INTEGRATED_PCF.DAT ]
 then
-   cp INTEGRATED_PCF.DAT \${SLURM_SUBMIT_DIR}/INTEGRATED_PCF_${out}.DAT
+   cp INTEGRATED_PCF.DAT \${SLURM_SUBMIT_DIR}/INTEGRATED_PCF_${job_id}.DAT
 fi
 
 if [ -e INTERPOL_PCF.DAT ]
 then
-   cp INTERPOL_PCF.DAT \${SLURM_SUBMIT_DIR}/INTERPOL_PCF_${out}.DAT
+   cp INTERPOL_PCF.DAT \${SLURM_SUBMIT_DIR}/INTERPOL_PCF_${job_id}.DAT
 fi
 
 if [ -e SCFOUT.LOG ]
 then
-   cp SCFOUT.LOG \${SLURM_SUBMIT_DIR}/${out}.out2
+   cp SCFOUT.LOG \${SLURM_SUBMIT_DIR}/${job_id}.out2
 fi
 
 if [ -e FREQUENCIES.DAT ]
 then
-   cp FREQUENCIES.DAT \${SLURM_SUBMIT_DIR}/FREQUENCIES_${out}.DAT
+   cp FREQUENCIES.DAT \${SLURM_SUBMIT_DIR}/FREQUENCIES_${job_id}.DAT
 fi
 
 if [ -e FREQPLOT.DAT ]
 then
-   cp FREQPLOT.DAT \${SLURM_SUBMIT_DIR}/FREQPLOT_${out}.DAT
+   cp FREQPLOT.DAT \${SLURM_SUBMIT_DIR}/FREQPLOT_${job_id}.DAT
 fi
 
 if [ -e HESSFREQ.DAT ]
 then
-   cp HESSFREQ.DAT \${SLURM_SUBMIT_DIR}/${out}.hessfreq
+   cp HESSFREQ.DAT \${SLURM_SUBMIT_DIR}/${job_id}.hessfreq
 fi
----------------------------------------------------- " >> ${out}.sbatch
+---------------------------------------------------- " >> ${job_id}.sbatch
 
-
-sbatch ${out}.sbatch
+fi
+sbatch ${job_id}.sbatch
