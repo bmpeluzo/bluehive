ffb629c8beacc0e05b1cf3e94d71fb9858d25fc8
Barbara M. T. C. Peluzo <bmpeluzo.smu@gmail.com>
2024-07-19 12:44:39 -0400

    DFT option implemented

--- get_par_data.py
+++ get_par_data.py
@@ -55,21 +55,27 @@ import numpy as np
 
 path='/home/bteixeir/parallel_tests/'
 part='teraeth'
-n_nodes=2
+n_nodes=1
 sys='ice'
-list_calc=['sp', 'opt']#, 'freq']
+list_calc=['sp', 'opt', 'freq']
 list_vers=['barbara']
+dft='pbe'
+
+if dft == 'b3lyp':
+    name=sys
+else:
+    name=sys+'_'+dft
 
 for vers in list_vers:
     
     for calc in list_calc:
-        out_file=open(path+'results_%s_%snode_%s_%s_%s.dat'%(part,n_nodes,sys,vers,calc),'w+')
+        out_file=open(path+'results_%s_%snode_%s_%s_%s.dat'%(part,n_nodes,name,vers,calc),'w+')
         out_file.write('n_nodes\tn_tasks_per_node\tn_cores\tn_cyc\tt_elapse\tt_cpu\tpar_eff\n')
-        tex_file=open(path+'results_%s_%snode_%s_%s_%s.tex'%(part,n_nodes,sys,vers,calc),'w+')
-        t_serial=get_telapse(path+'%s/1node/%s/%s/%s_%s_1.out'%(part,sys,vers,sys,calc))
+        tex_file=open(path+'results_%s_%snode_%s_%s_%s.tex'%(part,n_nodes,name,vers,calc),'w+')
+        t_serial=get_telapse(path+'%s/1node/%s/%s/%s_%s_1.out'%(part,sys,vers,name,calc))
         for i in np.arange(4,68,4):
             print('%d cores' %i)
-            cry_out=path+'%s/%snode/%s/%s/%s_%s_%s.out'%(part,n_nodes,sys,vers,sys,calc,i)
+            cry_out=path+'%s/%snode/%s/%s/%s_%s_%s.out'%(part,n_nodes,sys,vers,name,calc,i)
             telapse=get_telapse(cry_out=cry_out)
             tcpu=get_tcpu(cry_out=cry_out)
             if calc == 'opt':
